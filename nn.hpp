f30d0e5bd98f71b4b415c0c42ee1d76a1926c652
K Ernest (iFire) Lee <fire@chibifire.com>
2013-10-31 05:25:31 -0700

    Add nstr_send and nstr_recv.

--- nn.hpp
+++ nn.hpp
@@ -29,6 +29,7 @@
 #include <cstring>
 #include <algorithm>
 #include <exception>
+#include <stdarg.h>
 
 #if defined __GNUC__
 #define nn_slow(x) __builtin_expect ((x), 0)
@@ -180,6 +181,64 @@ namespace nn
             }
             return rc;
         }
+
+		/*  Use a string formatted with format strings and return new buffer. 
+			If there was not enough memory, returns NULL. */
+		inline char * s_vprintf (const char *format, va_list argptr)
+		{
+			int size = 256;
+			char * string = (char *) malloc (size);
+			if(!string) {
+			  return NULL;
+			}
+			int required = vsnprintf (string, size, format, argptr); 
+		#if defined (__WINDOWS__)
+			if (required < 0 || required >= size)
+				required = _vscprintf (format, argptr);
+		#endif
+			//  If the string does not fit, allocate a larger buffer for it.
+			if (required >= size) {
+				size = required + 1;
+
+				char * temp = (char *) realloc (string, size);
+				if (!temp) {
+					free(string);
+					return NULL;
+				}
+				string = temp;
+        
+				vsnprintf (string, size, format, argptr);
+			}
+			return string;
+		}
+
+		/* Send C string. Returns 0 if successful, -1 if there was an failure. */
+		inline int nstr_send (const char *format, ...)
+		{
+			assert (format);
+    
+			va_list argptr;
+			va_start (argptr, format);
+			char *string = s_vprintf (format, argptr);
+			va_end (argptr);
+
+			int rc = this->send(string, NN_MSG, 0); 
+			assert (rc == sz_msg);
+			return rc;
+		}
+
+		/* Get C string. Returns NULL if there is an error. 
+		   Must call nn_freemsg(buf) after use */
+		inline char * nstr_recv ()
+		{
+			void *buf = NULL;
+			const int nbytes = this->recv(&buf, NN_MSG, 0);
+			assert (nbytes >= 0);
+			char *string = (char *) malloc (nbytes + 1);
+			memcpy (string, &buf, nbytes);
+			nn_freemsg (buf);
+			return string;
+		}
 
     private:
 

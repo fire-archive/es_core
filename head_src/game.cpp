e915c689665ebd324fb9c8543254a9c184675734
K Ernest (iFire) Lee <fire@chibifire.com>
2013-12-01 00:59:24 -0800

    Fully switch to c++ random.

--- head_src/game.cpp
+++ head_src/game.cpp
@@ -65,8 +65,10 @@ void game_init( GameThreadSockets & gsockets, GameState & gs, SharedRenderState 
   std::mt19937 rng;
   rng.seed( (unsigned long)now );
   gs.bounce = 25.0f;
-  gs.speed = (float)( rand() % 60 + 40 );
-  float angle = (float)( rand() % 360 ) * 2.0f * (float)M_PI / 360.0f;
+  std::uniform_real_distribution<float> rand_speed(1, 60);
+  gs.speed = rand_speed(rng) + 40;
+  std::uniform_real_distribution<float> rand_angle(1, 360);
+  float angle = rand_angle(rng) * 2.0f * (float)M_PI / 360.0f;
   gs.direction = Ogre::Vector2( cosf( angle ), sinf( angle ) );
   rs.orientation = Ogre::Quaternion( Ogre::Radian( 0.0f ), Ogre::Vector3::UNIT_Z );
   rs.position = Ogre::Vector3::ZERO;
